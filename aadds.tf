aec4638d7af4eda0eecfe5bfae0149d6911ba120
Michael Schnerring <3743342+schnerring@users.noreply.github.com>
2022-02-20 04:34:51 +0100

    Add Microsoft.AAD resource provider registration

--- aadds.tf
+++ aadds.tf
@@ -2,3 +2,7 @@
 resource "azuread_service_principal" "aadds" {
   application_id = var.domain_controller_services_id
 }
+
+resource "azurerm_resource_provider_registration" "aadds" {
+  name = "Microsoft.AAD"
+}
