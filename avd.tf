8340634537d13ecd81ec34469d3ca0c2ad81fea2
Michael Schnerring <3743342+schnerring@users.noreply.github.com>
2022-02-22 02:43:02 +0100

    Add AVD rg, vnet, snet

---
+++ avd.tf
@@ -0,0 +1,26 @@
+
+# Resource Group
+
+resource "azurerm_resource_group" "avd" {
+  name     = "avd-rg"
+  location = var.location
+}
+
+# Network Resources
+
+resource "azurerm_virtual_network" "avd" {
+  name                = "avd-vnet"
+  location            = azurerm_resource_group.avd.location
+  resource_group_name = azurerm_resource_group.avd.name
+  address_space       = ["10.10.0.0/16"]
+
+  # Use AADDS DCs as DNS servers
+  dns_servers = azurerm_active_directory_domain_service.aadds.initial_replica_set.0.domain_controller_ip_addresses
+}
+
+resource "azurerm_subnet" "avd" {
+  name                 = "avd-snet"
+  resource_group_name  = azurerm_resource_group.avd.name
+  virtual_network_name = azurerm_virtual_network.avd.name
+  address_prefixes     = ["10.10.0.0/24"]
+}
