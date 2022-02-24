8a1a1c3525043ab2b831732c4db677787fc36d2b
Michael Schnerring <3743342+schnerring@users.noreply.github.com>
2022-02-24 04:21:33 +0100

    Add AVD host pool

--- avd.tf
+++ avd.tf
@@ -38,3 +38,31 @@ resource "azurerm_virtual_network_peering" "avd_to_aadds" {
   virtual_network_name      = azurerm_virtual_network.avd.name
   remote_virtual_network_id = azurerm_virtual_network.aadds.id
 }
+
+# Host Pool
+
+locals {
+  # Switzerland North is not supported
+  avd_location = "West Europe"
+}
+
+resource "azurerm_virtual_desktop_host_pool" "avd" {
+  name                = "vis-avd-hp"
+  location            = local.avd_location
+  resource_group_name = azurerm_resource_group.avd.name
+
+  type                = "Pooled"
+  load_balancer_type  = "BreadthFirst"
+  friendly_name       = "AVD Host Pool using AADDS"
+  start_vm_on_connect = true
+}
+
+resource "time_rotating" "avd_registration_expiration" {
+  # Must be between 1 hour and 30 days
+  rotation_days = 29
+}
+
+resource "azurerm_virtual_desktop_host_pool_registration_info" "avd" {
+  hostpool_id     = azurerm_virtual_desktop_host_pool.avd.id
+  expiration_date = time_rotating.avd_registration_expiration.rotation_rfc3339
+}
