be3f16d527c8ecc454e75eb610707e1a2de811c8
Michael Schnerring <3743342+schnerring@users.noreply.github.com>
2022-02-24 02:24:58 +0100

    Add network peerings

--- avd.tf
+++ avd.tf
@@ -24,3 +24,17 @@ resource "azurerm_subnet" "avd" {
   virtual_network_name = azurerm_virtual_network.avd.name
   address_prefixes     = ["10.10.0.0/24"]
 }
+
+resource "azurerm_virtual_network_peering" "aadds_to_avd" {
+  name                      = "hub-to-avd-peer"
+  resource_group_name       = azurerm_resource_group.aadds.name
+  virtual_network_name      = azurerm_virtual_network.aadds.name
+  remote_virtual_network_id = azurerm_virtual_network.avd.id
+}
+
+resource "azurerm_virtual_network_peering" "avd_to_aadds" {
+  name                      = "avd-to-aadds-peer"
+  resource_group_name       = azurerm_resource_group.avd.name
+  virtual_network_name      = azurerm_virtual_network.avd.name
+  remote_virtual_network_id = azurerm_virtual_network.aadds.id
+}
